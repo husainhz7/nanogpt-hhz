c167fae674a0aba018816bd61601a280f4fdc9d4
Husain Zaidi <husainhz7@gmail.com>
2023-02-05 21:46:24 +0530

    completing the transformer

--- gpt.py
+++ gpt.py
@@ -3,16 +3,19 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 32 # how many independent sequences will we process in parallel?
-block_size = 8 # what is the maximum context length for predictions?
+batch_size = 64 # how many independent sequences will we process in parallel?
+block_size = 256 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 300
-learning_rate = 1e-3
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embed = 32
+n_embed = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
 # ------------
-
+print(device)
 torch.manual_seed(1337)
 
 # curl https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt --output input.txt
@@ -65,6 +68,7 @@ class Head(nn.Module):
         self.query = nn.Linear(n_embed, head_size, bias=False)
         self.value = nn.Linear(n_embed, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         B, T, C = x.shape
@@ -75,6 +79,7 @@ class Head(nn.Module):
         wei= wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
         # aggregate 
         wei = F.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
         v = self.value(x)
         out = wei @ v
         return out
@@ -84,19 +89,22 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(n_embed,n_embed)
+        self.dropout = nn.Dropout(dropout)
+
 
     def forward(self,x):
         out = torch.cat([h(x) for h in self.heads], dim=-1)
-        out = self.proj(out)
+        out = self.dropout(self.proj(out))
         return out
 
 class FeedForward(nn.Module):
     def __init__(self, n_embed):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(n_embed, 4+n_embed), 
+            nn.Linear(n_embed, 4 * n_embed), 
             nn.ReLU(),
-            nn.Linear(4+n_embed,n_embed), 
+            nn.Linear(4 * n_embed, n_embed), 
+            nn.Dropout(dropout),
         )
 
     def forward(self, x):
@@ -118,18 +126,15 @@ class Block(nn.Module):
         return x
 
 # simple bigram model
-class BigramLanguageModel(nn.Module):
+class GPTModel(nn.Module):
 
     def __init__(self):
         super().__init__()
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
         self.position_embedding_table =  nn.Embedding(block_size, n_embed)
-        self.blocks = nn.Sequential(
-            Block(n_embed, n_head=4),
-            Block(n_embed, n_head=4),
-            Block(n_embed, n_head=4),
-        )
+        self.blocks = nn.Sequential(*[Block(n_embed, n_head) for _ in range(n_layer)])
+        self.layer_norm = nn.LayerNorm(n_embed)
         self.lm_head = nn.Linear(n_embed, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -140,6 +145,7 @@ class BigramLanguageModel(nn.Module):
         pos_emb = self.position_embedding_table(torch.arange(T, device=device))
         x = tok_emb + pos_emb # B T C
         x = self.blocks(x) # attention decoder block
+        x = self.layer_norm(x)
         logits = self.lm_head(x) #B,T,vocabSize
 
         if targets is None:
@@ -169,7 +175,7 @@ class BigramLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
         return idx
 
-model = BigramLanguageModel()
+model = GPTModel()
 m = model.to(device)
 
 # create a PyTorch optimizer
@@ -190,6 +196,8 @@ for iter in range(max_iters):
     optimizer.zero_grad(set_to_none=True)
     loss.backward()
     optimizer.step()
+
+torch.save(m.state_dict(), "nano-shkspr-gpt")
 
 # generate from the model
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
